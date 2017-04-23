7d430531c160c5922d3ea37dac5fd6c8fe12155a
wangliu <wangliu@isoftstone.com>
2017-04-23 15:52:42 +0800

    modified the content.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  users: any[] = [
+    {
+      name: "小刘",
+      age: "32"
+    },
+    {
+      name: "小蔡",
+      age: "28"
+    }
+  ];
 
   constructor(public navCtrl: NavController) {
 
