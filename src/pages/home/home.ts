ea897a19a3f47cbbfe6346d5e8931bed8b735dca
wangliu <wangliu@isoftstone.com>
2017-04-23 21:10:44 +0800

    添加alert组件

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,14 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, AlertController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+
+  }
 
   users: any[] = [
     {
@@ -15,11 +18,40 @@ export class HomePage {
     {
       name: "小蔡",
       age: "28"
+    },
+    {
+      name: "小王",
+      age: "32"
+    },
+    {
+      name: "小李",
+      age: "28"
+    },
+    {
+      name: "小张",
+      age: "32"
+    },
+    {
+      name: "小白",
+      age: "28"
+    },
+    {
+      name: "小马",
+      age: "28"
     }
   ];
 
-  constructor(public navCtrl: NavController) {
-
+  /**
+   *
+   * @summary 提示
+   * */
+  showAlert(message:string): void {
+    let alert = this.alertCtrl.create({
+      title: "提示",
+      message: message,
+      buttons: ["ok"]
+    });
+    alert.present();
   }
 
 }
